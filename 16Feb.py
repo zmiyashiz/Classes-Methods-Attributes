43c8657b4f2e28eb8d11c4fdd1332e6d485fc8b3
Timur <bazarsadaev06@mail.ru>
2026-02-16 10:58:49 +0800

    First commit

---
+++ 16Feb.py
@@ -0,0 +1,40 @@
+#Timur ID:2025088
+
+class Item:
+    def __init__(self, name, price):
+        self.name = name 
+        self.price = price 
+
+
+class ShoppingCart:
+    def __init__(self):
+        self.items = []
+
+    def showItems(self):
+        for i in self.items:
+            print(i.name)    
+    
+    def addItem(self, Item):
+        self.items.append(Item)
+    
+    def removeItem(self,Item):
+        self.items.remove(Item)
+      
+
+
+    def calculateTotal(self):
+        sum = 0
+        for i in self.items:
+            sum+=i.price
+        print(sum)
+
+item1 = Item("Cola", 3000) 
+item2 = Item("Cheeps", 6000)    
+      
+cart1 = ShoppingCart()
+cart1.addItem(item1)
+cart1.addItem(item2)
+cart1.removeItem(item1)
+cart1.showItems()
+cart1.calculateTotal()
+
